263aba958f65d5b80b0c4bf6d0defd6872c46501
a.boev.it@gmail.com <#gi32@+1>
2022-10-06 12:34:02 +0300

    Добавлены TODO

--- AvaloniaShikimoriApp/Program.cs
+++ AvaloniaShikimoriApp/Program.cs
@@ -4,6 +4,16 @@ using System;
 
 namespace AvaloniaShikimoriApp
 {
+    // TODO: 1. Сделать возможность открывать окна внутри одного приложения
+    // TODO: 2. Фильтрация по имени
+    // TODO: 3. Фильтрация: 1. Годы 2. Продолжительность
+    // TODO: 4. Сортировка всего списка + Не работает сортировка по имени
+    // TODO: 5. Изучить авторизацию
+    // TODO: 6. Пагинация
+    // TODO: 7. 
+
+
+    // TODO: 999: Получить длительность всех аниме в МИРЕ.
     class Program
     {
         // Initialization code. Don't use any Avalonia, third-party APIs or any
