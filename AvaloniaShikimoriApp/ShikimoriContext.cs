d1528ea7d35f536c378b86c69d86a67ea274f67c
a.boev.it@gmail.com <#gi32@+1>
2022-10-06 12:06:50 +0300

    Initial commit

---
+++ AvaloniaShikimoriApp/ShikimoriContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using AvaloniaShikimoriApp.Models;
+
+namespace AvaloniaShikimoriApp;
+
+public class ShikimoriContext
+{
+    private const string URL = "https://shikimori.one/";
+    private HttpClient httpClient;
+
+    public ShikimoriContext()
+    {
+        httpClient = new HttpClient();
+        httpClient.BaseAddress = new Uri(URL);
+    }
+
+    public List<Anime>? GetAnimes()
+    {
+        // season
+        // page
+        string param = "api/animes?limit=50&page=1&order=ranked";
+        List<Anime>? animes = new List<Anime>();
+        HttpResponseMessage response = httpClient.GetAsync(param).Result;
+        if (response.IsSuccessStatusCode)
+        {
+            string json = response.Content.ReadAsStringAsync().Result;
+            animes = JsonSerializer.Deserialize<List<Anime>>(json); 
+        }
+
+        return animes;
+    }
+}
